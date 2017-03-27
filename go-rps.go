974fa3126aafae9bdc894c1d6ddaca29a3193448
jbfink <john.fink@gmail.com>
2017-03-27 13:40:01 -0400

    Initial commit.

---
+++ go-rps.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("enter guess: ")
+	guess, _ := reader.ReadString('\n')
+	fmt.Println(guess)
+}
